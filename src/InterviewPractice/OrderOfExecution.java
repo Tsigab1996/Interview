02038ccb081fb61441eb465ff4c3bd8c64aa6470
Tsigab <107885237+Tsigab1996@users.noreply.github.com>
2023-04-07 07:01:41 -0500

    Initial commit

---
+++ src/InterviewPractice/OrderOfExecution.java
@@ -0,0 +1,19 @@
+package InterviewPractice;
+
+public class OrderOfExecution {
+
+     public OrderOfExecution(){
+         System.out.println("This is the constructor for the order of execution");
+     }
+
+    static {
+        System.out.println("This is a static block");
+    }
+    {
+        System.out.println("This is not static block or simply it is init block");
+    }
+    public static void main(String[] args) {
+
+         OrderOfExecution a= new OrderOfExecution();
+    }
+}
